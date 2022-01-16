0c02717d7aa3a373b94412ce7e4183291b3a78f3
Celia <celiamdona@gmail.com>
2022-01-16 11:11:12 -0500

    reduced code for character type validation

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -20,28 +20,28 @@ function generatePassword(){
   var password = "";  
   var passwordLenght = validateLenght();  
   if (passwordLenght){    
-  var confirmSpecial = confirmSpecialChar();
-  var confirmNum = confirmNumChar();
-  var confirmLower = confirmLowerChar();
-  var confirmUpper = confirmUpperChar();
+    var confirmSpecial = window.confirm("Click OK to confirm including special characters");
+    var confirmNum = window.confirm("Click OK to confirm including numeric characters");
+    var confirmLower = window.confirm("Click OK to confirm including lower case characters");
+    var confirmUpper = window.confirm("Click OK to confirm including upper case characters");
    if (confirmSpecial || confirmNum || confirmLower || confirmUpper){
       for (i=0 ; i< passwordLenght; ){
-       if (confirmSpecial == true){
+       if (confirmSpecial = true){
          var randomNumber = Math.floor(Math.random() * specialChar.length);
          password += specialChar.substring(randomNumber, randomNumber +1);
          i++;
         }
-       if (confirmNum == true && i < passwordLenght){
+       if (confirmNum = true && i < passwordLenght){
          var randomNumber = Math.floor(Math.random() * numericChar.length);
          password += numericChar.substring(randomNumber, randomNumber +1);
          i++;
         }
-       if (confirmUpper == true && i < passwordLenght){
+       if (confirmUpper = true && i < passwordLenght){
          var randomNumber = Math.floor(Math.random() * upperCaseChar.length);
          password += upperCaseChar.substring(randomNumber, randomNumber +1);
          i++;
         }
-       if (confirmLower == true && i < passwordLenght){
+       if (confirmLower = true && i < passwordLenght){
          var randomNumber = Math.floor(Math.random() * lowerCaseChar.length);
          password += lowerCaseChar.substring(randomNumber, randomNumber +1);
          i++;
@@ -78,47 +78,6 @@ function validateLenght(){
   }
   else {
       return passwordLenght;  
-  }
-}
-// Functions to Validate Character Types (can be improved, reduce # of functions)
-// Function to validate special character
-function confirmSpecialChar(){
-  var confirmSpecial = window.confirm("Click OK to confirm including special characters");
-  if (confirmSpecial){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-// Function to validate numeric character
-function confirmNumChar(){
-  var confirmNum = window.confirm("Click OK to confirm including numeric characters");
-  if (confirmNum){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-// Function to validate lower case character
-function confirmLowerChar(){
-  var confirmLower = window.confirm("Click OK to confirm including lower case characters");
-  if (confirmLower){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-// Function to validate upper case character
-function confirmUpperChar(){
-  var confirmUpper = window.confirm("Click OK to confirm including upper case characters");
-  if (confirmUpper){
-    return true;
-  }
-  else{
-    return false;
   }
 }
 
