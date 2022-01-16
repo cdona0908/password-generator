aaa2110d1633564d6471b1aaaa651d50d7fe1da9
Celia <celiamdona@gmail.com>
2022-01-16 11:06:31 -0500

    recode character type validation and value generator

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,54 +10,60 @@ var passwordMaxLenght = 128;
 var password = "";
 // Write password to the #password input
 function writePassword() {
+      
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+  debugger;  
   passwordText.value = password;
+  return;
+  
 }
 // Function to generate Password
 function generatePassword(){
   var password = "";  
   var passwordLenght = validateLenght();  
   if (passwordLenght){    
-  var confirmSpecial = confirmSpecialChar();
-  var confirmNum = confirmNumChar();
-  var confirmLower = confirmLowerChar();
-  var confirmUpper = confirmUpperChar();
+    var confirmSpecial = window.confirm("Click OK to confirm including special characters");
+    var confirmNum = window.confirm("Click OK to confirm including numeric characters");
+    var confirmLower = window.confirm("Click OK to confirm including lower case characters");
+    var confirmUpper = window.confirm("Click OK to confirm including upper case characters");
+  
    if (confirmSpecial || confirmNum || confirmLower || confirmUpper){
       for (i=0 ; i< passwordLenght; ){
-       if (confirmSpecial == true){
-         var randomNumber = Math.floor(Math.random() * specialChar.length);
-         password += specialChar.substring(randomNumber, randomNumber +1);
-         i++;
+        
+       if (confirmSpecial = true){
+         password = randomValue(specialChar);         
         }
        if (confirmNum == true && i < passwordLenght){
-         var randomNumber = Math.floor(Math.random() * numericChar.length);
-         password += numericChar.substring(randomNumber, randomNumber +1);
-         i++;
+         password = randomValue(numericChar);         
         }
        if (confirmUpper == true && i < passwordLenght){
-         var randomNumber = Math.floor(Math.random() * upperCaseChar.length);
-         password += upperCaseChar.substring(randomNumber, randomNumber +1);
-         i++;
+         password = randomValue(upperCaseChar);         
         }
        if (confirmLower == true && i < passwordLenght){
-         var randomNumber = Math.floor(Math.random() * lowerCaseChar.length);
-         password += lowerCaseChar.substring(randomNumber, randomNumber +1);
-         i++;
+         password = randomValue(lowerCaseChar);         
         }        
       }
       return password;           
     }
     else{
     window.alert("You must select at least one type of character");
-    return;
+    return null;
    }
   }
   else{
     return null;
   }  
 };
+
+// Function for Randmom character generation
+
+function randomValue(charType){
+  var randomNumber = Math.floor(Math.random() * charType.length);
+  password += charType.substring(randomNumber, randomNumber +1);
+  i++;
+  return password;
+}
 
 //Function for Password Lenght Validation
 
@@ -78,47 +84,6 @@ function validateLenght(){
   }
   else {
       return passwordLenght;  
-  }
-}
-// Functions to Validate Character Types (can be improved, reduce # of functions)
-// Function to validate special character
-function confirmSpecialChar(){
-  var confirmSpecial = window.confirm("Click OK to confirm including special characters");
-  if (confirmSpecial){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-// Function to validate numeric character
-function confirmNumChar(){
-  var confirmNum = window.confirm("Click OK to confirm including numeric characters");
-  if (confirmNum){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-// Function to validate lower case character
-function confirmLowerChar(){
-  var confirmLower = window.confirm("Click OK to confirm including lower case characters");
-  if (confirmLower){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-// Function to validate upper case character
-function confirmUpperChar(){
-  var confirmUpper = window.confirm("Click OK to confirm including upper case characters");
-  if (confirmUpper){
-    return true;
-  }
-  else{
-    return false;
   }
 }
 
